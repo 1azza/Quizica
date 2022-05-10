01958ab927561360ff9a28bddc3806c94dc72ce9
Larry Rennoldson <92909352+1azza@users.noreply.github.com>
2022-05-10 23:40:54 +0100

    Cleaner

--- src/QuizletAPI/user.py
+++ src/QuizletAPI/user.py
@@ -16,21 +16,8 @@ class User():
         payload = "{\"password\":\""+self.password+"\",\"username\":\""+self.username+"\"}"
         self.headers = {
             'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:101.0) Gecko/20100101 Firefox/101.0",
-            'Accept': "application/json",
-            'Accept-Language': "en-GB,en;q=0.5",
-            'Referer': "https://quizlet.com/login?redir=https://quizlet.com/latest",
             'CS-Token': "G2FdR2FnEsVKpwtTMGuG2q",
-            'x-requested-with': "XMLHttpRequest",
-            'Content-Type': "application/json",
-            'Origin': "https://quizlet.com",
-            'Connection': "keep-alive",
             'Cookie': cookies,
-            'Sec-Fetch-Dest': "empty",
-            'Sec-Fetch-Mode': "cors",
-            'Sec-Fetch-Site': "same-origin",
-            'Pragma': "no-cache",
-            'Cache-Control': "no-cache",
-            'TE': "trailers"
             }
         response = requests.request("POST", url, data=payload, headers=self.headers, params=querystring)
         if response.status_code == 200:
