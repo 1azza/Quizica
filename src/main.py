4c40443ca1b9a5ed52b54e5c6972550ffecac337
Larry Rennoldson <92909352+1azza@users.noreply.github.com>
2022-05-11 17:53:16 +0100

    Cleaner

--- src/main.py
+++ src/main.py
@@ -3,29 +3,37 @@ from pprint import pprint
 import logging
 import time
 import seneca
+import webbrowser
+client = Quizlet('larryrennoldson', 'Larry1102')
+user = seneca.User('017437@brgsmail.org.uk', 'Larry1102')
+MySet = client.Set(client.user.createSet())
 
-client = Quizlet('USERNAME', 'PASSWORD')
-user = seneca.User('USRENAME', 'PASSWORD')
 FOLDERS = {
     'Biology': 104164961,
 }
 url = input('Course URL:')
+print('Downloading course...')
 data = seneca.course.getCourseInfo(url, user)
+print('Generating flashcards...')
 NumberOfModules = 0
 Cards = {}
 title = data.get('title').replace(' HyperFlashcards', '')
 contents = data.get('contents')
 Question = 0
+print('Generating flashcards...')
 for i in contents:
     contentModules = i.get('contentModules')
     for i in contentModules:
         if i.get('moduleType') == "hyper-flashcard":
             content = i.get('content')
             Cards[content.get('question')] = content.get('answer')
-                 
-MySet = client.Set(client.user.createSet())
 MySet.addCards(Cards)
+print('Publishing set...')
 MySet.setTitle(title)
-print(MySet.publish())
+link = MySet.publish()
 MySet.saveToFolder(FOLDERS['Biology'])
-input('Press ENTER to exit')
+print(link)
+x = input('Press 1 to open in browser...')
+if x == '1':   
+ webbrowser.open_new(link)   
+input('Press ENTER to exit...')
